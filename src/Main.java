f84d2d588a85f17594c267dbcf9fa4765f526e5c
Ilya Maximov <xc.navi@gmail.com>
2022-08-31 13:02:24 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to  \"Ogre cave game\"!.\n Adventure awaits you!");
+        System.out.println("What is the name of your hero:");
+    }
+}
+
+
